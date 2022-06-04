444ecbcf4da1486ca7a2f0c7c3bbb813ec7b8405
Ranomenjanahary <hei.jaonary@gmail.com>
2022-06-04 15:15:16 +0300

    fixed: remove id_ville column

--- InsertDummyData.sql
+++ InsertDummyData.sql
@@ -1,14 +1,14 @@
 -- insert ville
-insert into ville (id_ville, nom_ville) values ('Antananarivo');
-insert into ville (id_ville, nom_ville) values ('Brickaville');
-insert into ville (id_ville, nom_ville) values ('Tamatave');
-insert into ville (id_ville, nom_ville) values ('Antsiranana');
-insert into ville (id_ville, nom_ville) values ('Toliara');
-insert into ville (id_ville, nom_ville) values ('Majunga');
-insert into ville (id_ville, nom_ville) values ('Foulpointe');
-insert into ville (id_ville, nom_ville) values ('Fianarantsoa');
-insert into ville (id_ville, nom_ville) values ('Mantasoa');
-insert into ville (id_ville, nom_ville) values ('Morondava');
+insert into ville (nom_ville) values ('Antananarivo');
+insert into ville (nom_ville) values ('Brickaville');
+insert into ville (nom_ville) values ('Tamatave');
+insert into ville (nom_ville) values ('Antsiranana');
+insert into ville (nom_ville) values ('Toliara');
+insert into ville (nom_ville) values ('Majunga');
+insert into ville (nom_ville) values ('Foulpointe');
+insert into ville (nom_ville) values ('Fianarantsoa');
+insert into ville (nom_ville) values ('Mantasoa');
+insert into ville (nom_ville) values ('Morondava');
 
 -- insert chauffeur
 insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('RAKOTONDRINA Paul', '033 12 365 15', 'ndrina@gmail.com', 1);
