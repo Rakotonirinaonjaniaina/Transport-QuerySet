053d9e5655b4f2a65108e042ee3360dc0777b989
Ranomenjanahary <hei.jaonary@gmail.com>
2022-06-04 15:12:09 +0300

    updateChauffeur

--- InsertDummyData.sql
+++ InsertDummyData.sql
@@ -1,5 +1,4 @@
 -- insert ville
-
 insert into ville (id_ville, nom_ville) values ('Antananarivo');
 insert into ville (id_ville, nom_ville) values ('Brickaville');
 insert into ville (id_ville, nom_ville) values ('Tamatave');
@@ -10,3 +9,15 @@ insert into ville (id_ville, nom_ville) values ('Foulpointe');
 insert into ville (id_ville, nom_ville) values ('Fianarantsoa');
 insert into ville (id_ville, nom_ville) values ('Mantasoa');
 insert into ville (id_ville, nom_ville) values ('Morondava');
+
+-- insert chauffeur
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('RAKOTONDRINA Paul', '033 12 365 15', 'ndrina@gmail.com', 1);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('KOTO Nato', '034 56 566 54', 'nato@gmail.com', 2);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('BAKO Harry', '033 12 895 52', 'harrylepapa@gmail.com', 3);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('KATY Ilo', '032 89 456 12', 'ilo@gmail.com', 4);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('ALAIN Botrix', '033 25 789 52', 'botrix@gmail.com', 5);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('MAMY Gervé', '033 26 369 98', 'gery@gmail.com', 6);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('Lissy Edelman', '038 59 698 23', 'lissy@gmail.com', 7);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('JONATHAN', '032 12 145 25', 'jo@gmail.com', 8);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('FANIRY Jess', '033 23 123 32', 'jess@gmail.com', 9);
+insert into chauffeur (nom_complet, contact_chauffeur, mail_chauffeur, id_vehicule) values ('ANDRY Fetra', '033 25 258 85', 'andry@gmail.com', 10);
