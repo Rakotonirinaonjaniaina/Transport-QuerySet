1cefa5ca8e5ccd5633952ebe73065ca2a1a3704c
Tsirimaholy <hei.tsirimaholy@gmail.com>
2022-06-06 16:00:03 +0300

    feat: :card_file_box: add column nom_maintenance to maintenance table

--- GenerateDatabase.sql
+++ GenerateDatabase.sql
@@ -82,5 +82,6 @@ CREATE TABLE reserver(
 CREATE TABLE requerir(
     id_maintenance int not null REFERENCES maintenance(id_maintenance) not null,
     id_vehicule int not null REFERENCES vehicules(id_vehicule) not null,
-    unique(id_maintenance,id_vehicule)
+    unique(id_maintenance,id_vehicule),
+    nom_maintenance VARCHAR(100)
 );
