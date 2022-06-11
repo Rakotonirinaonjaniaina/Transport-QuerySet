bd31c84b002b6aa521e65f3a26d8918350bc8297
Ranomenjanahary <99523507+Ranomenjanahary@users.noreply.github.com>
2022-06-11 18:31:05 +0300

    Update departJour.sql

--- queryData/departJour.sql
+++ queryData/departJour.sql
@@ -1,5 +1,4 @@
 -- afficher le depart du jour
 
-SELECT * FROM voyage 
-  WHERE heure_depart <= '06:00 AM' 
-  AND heure_depart >= '05:59 PM' ;
+SELECT * FROM voyage
+  WHERE heure_depart >= '05:59 PM';
