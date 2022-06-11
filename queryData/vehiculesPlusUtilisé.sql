e0ea9d22fd27f66db50850cdeb52f330f306a92d
P3RM1_D3N1ED <72337259+Tsirimaholy@users.noreply.github.com>
2022-06-11 13:56:07 +0300

    :art: :style: reformat code

--- queryData/vehiculesPlusUtilisé.sql
+++ queryData/vehiculesPlusUtilisé.sql
@@ -1,13 +1,12 @@
 --Afficher les vehicules les plus utilisés
-create view countTrip 
-as 
-select matricule,count (id_voyage) 
-from utiliser 
-    inner join vehicules 
-        on utiliser.id_vehicule = vehicules.id_vehicule 
+create view countTrip
+as
+select matricule, count(id_voyage)
+from utiliser
+         inner join vehicules
+                    on utiliser.id_vehicule = vehicules.id_vehicule
 group by matricule;
 
-
-select matricule 
-from countTrip 
-where count = (select max (count) from countTrip);
+select matricule
+from countTrip
+where count = (select max(count) from countTrip);
