be5361705e4bf3ff97621bdb9281720d29e7c23e
P3RM1_D3N1ED <72337259+Tsirimaholy@users.noreply.github.com>
2022-06-14 12:39:27 +0300

    Delete MatriculeParVoyage.sql

--- queryData/MatriculeParVoyage.sql
+++
@@ -1,7 +0,0 @@
--- sujet => "28- Les véhicules utilisés pour un voyage "
-select matricule , jour_depart from voyage
-inner join utiliser
-on utiliser.id_voyage = voyage.id_voyage
-inner join vehicules
-on vehicules.id_vehicule = utiliser.id_vehicule
-group by vehicules.matricule , voyage.jour_depart;
