621fbcb9c5986969cd0d00997aae7f5bb4c53213
fanjasoa <fanjasoa18@gmail.com>
2022-06-16 21:00:22 +0300

    fix: remove unecesssary file

--- queryData/PayementInsuffisant.sql
+++
@@ -1,7 +0,0 @@
---Combien de clients n'ont pas encore effectué leur payement en totalité?Lesquels?
-
-SELECT (c.nom_client ||''|| prenom_client) as nom_complet, o.type, o.tarif, reserver.montant_paye
-FROM reserver
-	INNER JOIN client c ON reserver.id_client = c.id_client
-	INNER JOIN offre o ON reserver.id_offre = o.id_offre
-where reserver.montant_paye < o.tarif;
